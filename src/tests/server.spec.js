5afae8a8660b86ce511c2694f3e129f5776eb5b9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-11 16:25:46 -0400

    added endpoints tests

--- src/tests/server.spec.js
+++ src/tests/server.spec.js
@@ -2,6 +2,10 @@ const request = require('supertest');
 const api = require('../../');
 const db = require('../database/dbConfig');
 
+afterEach(async () => {
+    await db('resources').truncate();
+});
+
 describe('----- Server -----', () => {
     describe('ROUTE /', () => {
         describe('GET', () => {
@@ -9,23 +13,160 @@ describe('----- Server -----', () => {
                 const response = await request(api).get('/resources');
                 expect(response.status).toBe(200);
             });
+
+            it('should respond with JSON', async () => {
+                const response = await request(api).get('/resources');
+                expect(response.type).toBe(/json/i);
+            });
+
+            it('should get the appropriate response object', async () => {
+                const response = await request(api).get('/resources');
+                expect(response.body).toEqual([]);
+            });
         });
 
         describe('POST', () => {
+            it('should get a response status code 201 on success', async () => {
+                const response = await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                expect(response.status).toBe(201);
+            });
 
+            it('should get a response status code 500 on failure', async () => {
+                const response = await request(api).post('/resources', {
+                    name: ''
+                });
+                expect(response.status).toBe(500);
+            });
+
+            it('should respond with JSON', async () => {
+                const response = await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                expect(response.type).toBe(/json/i);
+            });
+
+            it('should get the appropriate response', async () => {
+                const response = await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                expect(response.body).toEqual({
+                    id: 1
+                });
+            });
         });
     });
     describe('ROUTE /:id', () => {
         describe('GET', () => {
+            it('should get a response status code 200 on success', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).get('/resources/1');
+                expect(response.status).toBe(200);
+            });
 
+            it('should get a response status code 500 on failure', async () => {
+                const response = request(api).get('/resources/1');
+                expect(response.status).toBe(500);
+            });
+
+            it('should respond with JSON', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).get('/resources/1');
+                expect(response.type).toBe(/json/i);
+            });
+
+            it('should get the appropriate response', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).get('/resources/1');
+                expect(response.status).toBe(200);
+                expect(response.body).toEqual({
+                    name: 'resource name'
+                });
+            });
         });
 
         describe('PUT', () => {
+            it('should get a response status code 200 on success', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).put('/resources/1', {
+                    name: 'resouce name 2'
+                });
+                expect(response.status).toBe(200);
+            });
 
+            it('should get a response status code 500 on failure', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).put('/resources/1', {
+                    name: 'resouce name 2'
+                });
+                expect(response.status).toBe(500);
+            });
+
+            it('should respond with JSON', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).put('/resources/1', {
+                    name: 'resouce name 2'
+                });
+                expect(response.type).toBe(/json/i);
+            });
+
+            it('should get the appropriate response', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).put('/resources/1', {
+                    name: 'resouce name 2'
+                });
+                expect(response.body).toEqual({
+                    name: 'resource name 2'
+                });
+            });
         });
 
         describe('DELETE', () => {
+            it('should get a response status code 200 on success', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).delete('/resources/1');
+                expect(response.status).toBe(200);
+            });
 
+            it('should get a response status code 500 on failure', async () => {
+                const response = request(api).delete('/resources/1');
+                expect(response.status).toBe(500);
+            });
+
+            it('should respond with JSON', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).delete('/resources/1');
+                expect(response.type).toBe(/json/i);
+            });
+
+            it('should get the appropriate response', async () => {
+                await request(api).post('/resources', {
+                    name: 'resource name'
+                });
+                const response = request(api).delete('/resources/1');
+                expect(response.body).toEqual({
+                    success: true
+                });
+            });
         });
     });
 });
