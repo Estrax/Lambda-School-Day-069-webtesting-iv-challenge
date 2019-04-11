f69221713ce40a8afcb4218af57342f9c58e6a9b
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-11 17:15:45 -0400

    added empty routes

--- src/routes/resources.js
+++ src/routes/resources.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
+const knex = require('knex');
+const dbConfig = require('../../knexfile');
+const db = knex(dbConfig.development);
+
+router.route('/')
+    .get(async (req, res) => {
+
+    })
+    .post(async (req, res) => {
+        
+    });
+
+router.route('/:id')
+    .get(async (req, res) => {
+
+    })
+    .put(async (req, res) => {
+        
+    })
+    .delete(async (req, res) => {
+        
+    });
+
 
 module.exports = router;
