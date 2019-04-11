2e00171f420b67548750f985cb4b1d00baf08ac0
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-11 17:23:37 -0400

    changed mounting point for api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const resources = require('./resources');
 
-router.use('/', resources);
+router.use('/resources', resources);
 
 module.exports = router;
