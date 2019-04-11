bbb60d08c7196db2a070ca77fb8661e6061b8940
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-11 16:41:02 -0400

    updated resource model tests

--- src/tests/resourceModel.spec.js
+++ src/tests/resourceModel.spec.js
@@ -1,34 +1,46 @@
 const resources = require('../database/helpers/resourceHelpers');
 const request = require('supertest');
 
+afterEach(async () => {
+    await db('resources').truncate();
+});
+
 describe('----- RESOURCE MODEL -----', () => {
     describe('GET', async () => {
-        it('should return all resources on success', () => {
-
+        it('should return all resources on success', async () => {
+            const response = await resources.get();
+            expect(response).toEqual([]);
         });
     });
 
     describe('INSERT', async () => {
-        it('should insert a new resource on success', () => {
-
+        it('should insert a new resource on success', async () => {
+            const response = await resources.post({ name: 'resource name' });
+            expect(response).toEqual({ id: 1 });
         });
     });
 
     describe('GET WITH ID', async () => {
-        it('should get a resource with ID on success', () => {
-
+        it('should get a resource with ID on success', async () => {
+            await resources.post({ name: 'resource name' });
+            const response = await resources.get(1);
+            expect(response).toEqual({ id: 1, name: 'resource name' });
         });
     });
 
     describe('UPDATE', async () => {
-        it('should update a resource with ID on success', () => {
-
+        it('should update a resource with ID on success', async () => {
+            await resources.post({ name: 'resource name' });
+            const response = await resources.update(1, { name: 'resource name 2' });
+            expect(response).toEqual({ id: 1, name: 'resource name 2' });
         });
     });
 
     describe('REMOVE', async () => {
-        it('should remove a resource with ID on success', () => {
-
+        it('should remove a resource with ID on success', async () => {
+            await resources.post({ name: 'resource name' });
+            const response = await resources.remove(1)
+            expect(response).toEqual(1);
         });
     });
 });
