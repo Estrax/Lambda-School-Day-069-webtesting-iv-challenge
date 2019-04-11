13969ad9d9f06232d7f8a2a2d9226e638a4f2147
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-11 16:30:34 -0400

    added db tests

---
+++ src/tests/resourceModel.spec.js
@@ -0,0 +1,34 @@
+const resources = require('../database/helpers/resourceHelpers');
+const request = require('supertest');
+
+describe('----- RESOURCE MODEL -----', () => {
+    describe('GET', async () => {
+        it('should return all resources on success', () => {
+
+        });
+    });
+
+    describe('INSERT', async () => {
+        it('should insert a new resource on success', () => {
+
+        });
+    });
+
+    describe('GET WITH ID', async () => {
+        it('should get a resource with ID on success', () => {
+
+        });
+    });
+
+    describe('UPDATE', async () => {
+        it('should update a resource with ID on success', () => {
+
+        });
+    });
+
+    describe('REMOVE', async () => {
+        it('should remove a resource with ID on success', () => {
+
+        });
+    });
+});
