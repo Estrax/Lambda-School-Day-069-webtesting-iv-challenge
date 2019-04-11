7b271b5ecd9c7f917fefe800edd9a545121f035d
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-11 15:53:15 -0400

    added status code spec to GET /

--- src/tests/server.spec.js
+++ src/tests/server.spec.js
@@ -5,7 +5,10 @@ const db = require('../database/dbConfig');
 describe('----- Server -----', () => {
     describe('ROUTE /', () => {
         describe('GET', () => {
-
+            it('should get a response status code 200', async () => {
+                const response = await request(api).get('/resources');
+                expect(response.status).toBe(200);
+            });
         });
 
         describe('POST', () => {
